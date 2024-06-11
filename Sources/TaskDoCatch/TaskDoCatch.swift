d7aadf97f2d7dc321877f94d365187026532f438
Alex Nagy <rebeloperalpar@gmail.com>
2024-06-11 13:27:51 +0300

    first init

--- Sources/TaskDoCatch/TaskDoCatch.swift
+++ Sources/TaskDoCatch/TaskDoCatch.swift
@@ -1,2 +1,25 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public extension View {
+    func task(priority: TaskPriority = .userInitiated, do action: @escaping () async throws -> (), catch: @escaping (Error) -> ()) -> some View {
+        self.task(priority: priority) {
+            do {
+                try await action()
+            } catch {
+                `catch`(error)
+            }
+        }
+    }
+}
+
+public func Task(priority: TaskPriority? = nil, do operation: @escaping () async throws -> (), catch: @escaping (Error) -> ()) {
+    Task(priority: priority) {
+        do {
+            try await operation()
+        } catch {
+            `catch`(error)
+        }
+    }
+}
+
--- Tests/TaskDoCatchTests/TaskDoCatchTests.swift
+++ Tests/TaskDoCatchTests/TaskDoCatchTests.swift
@@ -1,12 +1,4 @@
 import XCTest
 @testable import TaskDoCatch
 
-final class TaskDoCatchTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
-}
+final class TaskDoCatchTests: XCTestCase { }
